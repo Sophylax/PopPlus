d8b51567112109d84d7ffd36e18067eeded3b220
Ulaş Sophylax Sert <sophylax@sophylax.net>
2021-09-29 19:26:11 +0300

    Add Timestamp Tracking to Database Library
    I might add a combined timeout checking retrieval function later.

--- background/lib/database.js
+++ background/lib/database.js
@@ -4,14 +4,17 @@
 //	There are a lot of information in popmundo that is very persistent so we keep them here to avoid re-parsing pages
 //	Like Configuration library, we keep a local copy for ease, updates reflect to local copy
 //	Unlike that we initialize everything and there is no default value
+//	We also keep track of when an item is updated, if the script needs some values to be expired
 
 database = {
 	//This is what this library maintains
-	localcopy: {},
+	values: {},
+	timestamps: {},
 
 	//Called once at setup, this thing populates our local copy from chrome.local
 	initalize: async function () {
-		this.localcopy = await promiseLocalGet(null);
+		this.values = await promiseLocalGet('database_values').catch(()=>({}));
+		this.timestamps = await promiseLocalGet('database_timestamps').catch(()=>({}));
 	},
 
 	//A lot of the times we involve this library to check if we know something already
@@ -33,13 +36,26 @@ database = {
 	//	get(k1,k2,...)
 	get: function () {
 		var keys = Array.from(arguments);
-		var value = this.localcopy;
+		var cursor = this.values;
 		for (const key of keys){
-			value = value[key];
-			if (value === undefined)
-				return value;
+			cursor = cursor[key];
+			if (cursor === undefined)
+				return cursor;
 		}
-		return value;
+		return cursor;
+	},
+
+	//Retreive timestamp for a local copy
+	//	getTimestamp(k1,k2,...)
+	getTimestamp: function () {
+		var keys = Array.from(arguments);
+		var cursor = this.timestamps;
+		for (const key of keys){
+			cursor = cursor[key];
+			if (cursor === undefined)
+				return cursor;
+		}
+		return cursor;
 	},
 
 	//Update local and chrome.local copy
@@ -47,16 +63,24 @@ database = {
 	set: function () {
 		var keys = Array.from(arguments);
 		var value = keys.pop();
-		var prev = this.localcopy;
+		var value_cursor = this.values;
+		var stamp_cursor = this.timestamps;
 		for (var i = 0; i < keys.length; i++) {
 			var key = keys[i];
 			if (i == keys.length - 1) {
-				prev[key] = value;
+				value_cursor[key] = value;
+				stamp_cursor[key] = new Date();
 			}
-			else if (prev[key] === undefined)
-				prev[key] = {};
-			prev = prev[key];
+			else if (value_cursor[key] === undefined) {
+				value_cursor[key] = {};
+			}
+			else if (stamp_cursor[key] === undefined) {
+				stamp_cursor[key] = {};
+			}
+			value_cursor = value_cursor[key];
+			stamp_cursor = value_cursor[key];
 		}
-		chrome.storage.local.set({[keys[0]]: this.localcopy[keys[0]]});
+		chrome.storage.local.set({'database_values': this.values});
+		chrome.storage.local.set({'database_timestamps': this.timestamps});
 	}
 }
