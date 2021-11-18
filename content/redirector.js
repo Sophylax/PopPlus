efa4efaf369331e22f6beae84f22991a8eb1d5bc
Ulaş "Sophylax" Sert <sophylax@sophylax.net>
2021-11-18 21:24:58 +0300

    Add Login Detection to Redirector

--- content/redirector.js
+++ content/redirector.js
@@ -1,3 +1,9 @@
-notification.addPageNotification(document, 'deneme')
+notification.addPageNotification(document, 'LOGIN?')
+if (parse.loginState(document)) {
+    notification.addPageNotification(document, 'YES!')
+} else {
+    notification.addPageNotification(document, 'NO!')
+}
+
 console.log(browser.extension)
 console.log(browser.runtime)
