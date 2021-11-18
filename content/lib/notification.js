52dfbbff3baa17f414cb42a5176aea69c5d63ba7
Ulaş "Sophylax" Sert <sophylax@sophylax.net>
2021-11-18 20:28:36 +0300

    Add Barebones Notification Library

---
+++ content/lib/notification.js
@@ -0,0 +1,10 @@
+//TODO: Document this
+
+notification = {
+    addPageNotification: function(document, text) {
+        var o = `<div class="notification-real notification-error">${text}</div>`;
+        document.getElementById('notifications').insertAdjacentHTML('beforeend', o)
+        document.querySelector("#notifications > div:last-child").style.filter = 'hue-rotate(210deg)'
+        document.querySelector("#notifications > div:first-child").hidden = true
+    }
+}
