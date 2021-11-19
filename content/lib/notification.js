3b1bb1bbd056b94da023ae4c0d6464f4d1c5eb99
Ulaş "Sophylax" Sert <sophylax@sophylax.net>
2021-11-19 17:06:08 +0300

    Return Notification when Added to Page

--- content/lib/notification.js
+++ content/lib/notification.js
@@ -6,5 +6,6 @@ notification = {
         document.getElementById('notifications').insertAdjacentHTML('beforeend', o)
         document.querySelector("#notifications > div:last-child").style.filter = 'hue-rotate(210deg)'
         document.querySelector("#notifications > div:first-child").hidden = true
+        return document.querySelector("#notifications > div:last-child")
     }
 }
