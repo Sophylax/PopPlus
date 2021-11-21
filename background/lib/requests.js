dd22d1d3f51e06a2c14e0813915791c0c82cd7c2
Ulaş "Sophylax" Sert <sophylax@sophylax.net>
2021-11-21 23:14:29 +0300

    Remove 'no-cors' from Default Requests

--- background/lib/requests.js
+++ background/lib/requests.js
@@ -3,8 +3,8 @@
 requests = {
 
     //Mostly used primitive network requests, simple GET and POST
-    get: (url) => fetch(url, { mode: 'no-cors' }),
-    post: (url, body) => fetch(url, { mode: 'no-cors', method: 'POST', body: body }),
+    get: (url) => fetch(url, {}),
+    post: (url, body) => fetch(url, { method: 'POST', body: body }),
 
     //We need to convert the response into DOM quite frequently, to easily parse the retrieved pages
     //	Works on promises, because everything in the JS hell has to be async
