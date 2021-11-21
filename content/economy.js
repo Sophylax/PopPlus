d6cc1ab553a631d32948b8c9d1b94a277a7437e2
Ulaş "Sophylax" Sert <sophylax@sophylax.net>
2021-11-21 23:15:14 +0300

    Add Bank Acount Totaler Script

---
+++ content/economy.js
@@ -0,0 +1,27 @@
+var economyPath = 'popmundo.com/World/Popmundo.aspx/Character/Economy';
+var isEconomy = document.URL.includes(economyPath);
+if (isEconomy) {
+    var nodes = document.querySelectorAll('#tableaccounts > tbody > tr > td.right > span');
+
+    if (nodes.length > 0) { //Better check if the table exists
+        var total = [].slice.call(nodes).map((x) => parseInt(x.innerText)).reduce((acc, el) => acc + el, 0) / 10;
+
+        var lastNode = document.querySelector("#tableaccounts > tbody > tr:last-child");
+        var totalNode = lastNode.cloneNode(true);
+
+        if (nodes.length % 2 === 0) {
+            totalNode.className = 'odd';
+        } else {
+            totalNode.className = 'even';
+        }
+
+        console.log(lastNode.classList)
+        console.log(totalNode.classList)
+
+        totalNode.children[0].innerHTML = '<b>Total:</b>';
+        totalNode.children[1].innerHTML = `<b>${total.toLocaleString('it', {minimumFractionDigits: 2})} M$</b>`;
+
+        lastNode.parentNode.parentNode.appendChild(totalNode);
+        //lastNode.parentNode.insertBefore(totalNode, lastNode.nextSibling);
+    }
+}
