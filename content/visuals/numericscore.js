94effb5fab08977c5241d90f24835ede236bc324
Ulaş "Sophylax" Sert <sophylax@sophylax.net>
2021-12-02 18:22:16 +0300

    Implement Adding Numbers to Scores

---
+++ content/visuals/numericscore.js
@@ -0,0 +1,13 @@
+//Add the numeric scores to each score link as they are added
+var numericScoreObserver = new window.MutationObserver(function(mutations) {
+    mutations.forEach(function(mutation) {
+        for (var i = 0; i < mutation.addedNodes.length; i++) {
+            var addedNode = mutation.addedNodes[i]
+            if (addedNode.tagName === "A" && addedNode.href.includes('/World/Popmundo.aspx/Help/Scoring/')) {
+                var score = addedNode.href.split('/World/Popmundo.aspx/Help/Scoring/')[1];
+                addedNode.innerText += ` (${score})`;
+            }
+        }
+    })
+});
+numericScoreObserver.observe(document.documentElement, { subtree: true, childList: true });
